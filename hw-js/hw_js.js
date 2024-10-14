7b0da77c49830b3453ac4ff7bbc0c55f0727fbe0
Suman <suman.mukherjee01@gmail.com>
2024-10-14 19:04:19 +0530

    hw-js-2

--- hw-js/hw_js.js
+++ hw-js/hw_js.js
@@ -31,3 +31,16 @@ function maxNumber(arr){
 }
 // console.log(maxNumber(numArr));
 
+function isPrime(num){
+    if(num <= 1) return false;
+    for(let i = 2; i<=Math.sqrt(num);i++){
+        if(num % i == 0){
+            return false;
+        }
+        return true;
+    }
+}
+function primeArray(arr){
+    return arr.filter(isPrime);
+}
+console.log(primeArray(numArr));
